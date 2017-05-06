595a1d024528942cacaa47c301c9ecc1586ccc3d
stephen-andrew-barry <stephen.andrew.barry@gmail.com>
2017-05-05 20:19:41 -0700

    style(views): adds views and begins styling

--- horse/views.py
+++ horse/views.py
@@ -2,11 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from models import Race
 
 # Create your views here.
 def main(request):
     races = Race.objects.all()
+    kentucky_derby = Race.objects.filter(race_name="Kentucky Derby")
 
     return render(request, 'horse/main.html', context={
         'races': races,
+        'kentucky_derby': kentucky_derby,
     })
